b6518b01234bbf906467aa74a0c3627508740e6c
denzap <denzap@bk.ru>
2020-10-28 17:57:35 +0300

    commit 15

--- homework1/src/main/java/com/example/homework1/MainActivity.kt
+++ homework1/src/main/java/com/example/homework1/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.homework1
 
+import android.content.res.Configuration
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            val txtView: TextView = findViewById(R.id.horizont)
+            txtView.visibility = View.VISIBLE
+        }
     }
+
 
     
 }
