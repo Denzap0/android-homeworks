8fde53fc9b599774c48faea176f6ce3644ce850d
denzap <denzap@bk.ru>
2020-11-02 14:10:57 +0300

    commit 3

--- homework5_2/src/main/java/com/example/homework5_2/MainActivity.kt
+++ homework5_2/src/main/java/com/example/homework5_2/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.homework5_2
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import android.view.View
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
